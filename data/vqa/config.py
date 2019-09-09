ef344a98fb73da6b5d102864e0afb88b565e98e5
ab3llini <albertomario.bellini@gmail.com>
2019-09-09 13:21:23 +0200

    Adding helpers

---
+++ data/vqa/config.py
@@ -0,0 +1,10 @@
+ROOT = '/opt/project'
+
+dataDir = ROOT + '/VQA'
+versionType = 'v2_'  # this should be '' when using VQA v2.0 dataset
+taskType = 'OpenEnded'  # 'OpenEnded' only for v2.0. 'OpenEnded' or 'MultipleChoice' for v1.0
+dataType = 'mscoco'  # 'mscoco' only for v1.0. 'mscoco' for real and 'abstract_v002' for abstract for v1.0.
+dataSubType = 'train2014'
+annFile = '%s/Annotations/%s%s_%s_annotations.json' % (dataDir, versionType, dataType, dataSubType)
+quesFile = '%s/Questions/%s%s_%s_%s_questions.json' % (dataDir, versionType, taskType, dataType, dataSubType)
+imgDir = '%s/Images/%s/' % (dataDir, dataSubType)
