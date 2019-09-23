9f5623fcac38f66ece6b24ae5237f62e877aab41
ab3llini <albertomario.bellini@gmail.com>
2019-09-23 18:17:30 +0200

    Fixing import errors for terminal

--- src/loaders/vqa.py
+++ src/loaders/vqa.py
@@ -5,6 +5,8 @@ this_path = os.path.dirname(os.path.realpath(__file__))
 root_path = os.path.abspath(os.path.join(this_path, os.pardir, os.pardir))
 sys.path.append(root_path)
 
+print(root_path)
+
 
 import os
 from vqaTools.vqa import VQA
