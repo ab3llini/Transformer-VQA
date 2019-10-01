d6f75bd8d8cd517fcefed33f03d78fa03d5c2dd9
ab3llini <albertomario.bellini@gmail.com>
2019-10-01 16:41:43 +0200

    Fixed imports for CLI running.

--- src/models/baseline/captioning/train.py
+++ src/models/baseline/captioning/train.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 this_path = os.path.dirname(os.path.realpath(__file__))
-root_path = os.path.abspath(os.path.join(this_path, os.pardir, os.pardir))
+root_path = os.path.abspath(os.path.join(this_path, os.pardir, os.pardir, os.pardir))
 sys.path.append(root_path)
 
 
